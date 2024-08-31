47286d4e6db6d65b249079251cce64270825da47
Rahul Baliyan <rahulbaliyan863@gmail.com>
2024-09-01 04:49:52 +0530

    setuped

---
+++ src/landing_page/home/Hero.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function Hero() {
+  return ( 
+    <h1>Hero<i class="fa fa-address-book" aria-hidden="true"></i></h1>
+   );
+}
+
+export default Hero;
---
+++ src/landing_page/products/ProductPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from '../Navbar';
+import Hero from './Hero';
+import Left from './Left';
+import Right from './Right';
+import Universe from './Universe';
+import Footer from '../Footer';
+function PricingPage() {
+  return ( 
+    <>
+      <Navbar />
+      <Hero />
+      <Left />
+      <Right />
+      <Universe />
+      <Footer />
+    </>
+   );
+}
+
+export default PricingPage;
